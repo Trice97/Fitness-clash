f5ad23a681b0e96d07302e385924289f28433c8a
Trice97 <bolinp@gmail.com>
2025-11-07 22:28:47 +0100

    correction d'erreurs disverses

--- backend/app/workouts.py
+++ backend/app/workouts.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, Boolean, ForeignKey, DateTime
 from sqlalchemy.sql import func
-from sqlalchemy.orm import relasionship
+from sqlalchemy.orm import relationship
 from app.database import Base
 
 
@@ -8,25 +8,27 @@ class Workout(Base):
     """table des entraînements générés automatiquement"""
     __tablename__ = "workouts"
 
-# 🔗 Lien vers l'utilisateur propriétaire du workout
-    user_id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
 
-# Niveau de difficulté du workout (lié au profil de l’utilisateur)
+    # 🔗 Lien vers l'utilisateur propriétaire du workout
+    user_id = Column(Integer, ForeignKey("users.id",ondelete="CASCADE"),nullable=False)
+
+    # Niveau de difficulté du workout (lié au profil de l’utilisateur)
     difficulty_level = Column(Integer, nullable=False)
 
-# statistiques
-total_points = Column(Integer, default=0)
-is_completed = Column(Boolean, default=False)
+    # statistiques
+    total_points = Column(Integer, default=0)
+    is_completed = Column(Boolean, default=False)
 
-created_at = Column(DateTime(timezone=True), server_default=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
-# relations ORM
-user = relationship("User", back_populates="workouts")
-workout_exercices = relasionship(
-    "WorkoutExercise",
-    back_populates="workout" \
-    "cascade="all, delete-orphan","
-    "order_by="WorkoutExercise.order"
+    # relations ORM
+    user = relationship("User", back_populates="workouts")
+    workout_exercises = relationship(
+        "WorkoutExercise",
+        back_populates="workout"
+        "cascade="all, delete-orphan"
+        "order_by="WorkoutExercise.order"
 )
 
 
@@ -35,13 +37,13 @@ def __repr__(self):
 
 class WorkoutExercise(Base):
     """Table d'association entre Workout et Exercise"""
-    __tablename__ = "workout_exercise"
+    __tablename__ = "workout_exercises"
 
     id = Column(Integer, primary_key=True, index=True)
 
     # clés étrangères
-    workout_id = Column(Integer, ForeignKey("workouts.id", onedelete="CASCADE"), nullable=False)
-    exercise_id = Column(Integer, ForeignKey("exercise.id", onedelete="CASCADE"), nullable=False)
+    workout_id = Column(Integer, ForeignKey("workouts.id", ondelete="CASCADE"), nullable=False)
+    exercise_id = Column(Integer, ForeignKey("exercises.id", ondelete="CASCADE"), nullable=False)
                                              
-def __repr__(self):
-    return f"<WorkoutExercise(workout_id={self.workout_id}, exercise_id={self.exercise_id}, order={self.order})>"
+    def __repr__(self):
+        return f"<WorkoutExercise(workout_id={self.workout_id}, exercise_id={self.exercise_id}, order={self.order})>"
