e03419f13ab549f26caa75ba3e4c596318c8835c
Trice97 <bolinp@gmail.com>
2025-11-08 22:42:25 +0100

    schemas-workout.py

---
+++ backend/app/schemas/workout.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import datetime
+from app.schemas.exercise import ExerciseResponse
+
+
+"""Schemas pour les entraînements (Workout)
+Définit les structures de données pour validation et réponse API.
+"""
+
